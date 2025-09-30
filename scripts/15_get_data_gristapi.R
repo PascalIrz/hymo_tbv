4c9cdc81965456bc5aaae709aff08842e0ddd12c
IRZ Pascal <pascal.irz@ofb.gouv.fr>
2025-09-30 10:34:27 +0200

    Add dirty code to clean the dataframe "operation"

--- scripts/15_get_data_gristapi.R
+++ scripts/15_get_data_gristapi.R
@@ -1,6 +1,8 @@
 library(gristapi)
 
-my_functions <- list.files(path = "R")
+my_functions <- list.files(path = "R", pattern = ".R", full.names = TRUE)
+map(.x = my_functions,
+    .f = source)
 
 # api_key <- Sys.getenv("GRIST_KEY")
 # doc_id <- Sys.getenv("GRIST_DOC_TEST")
@@ -11,21 +13,55 @@ api <- grist_api$new(
   doc_id = Sys.getenv("GRIST_DOC_ID")
 )
 
-api
+# api
 
 fetch_grist_db(api)
 
 
 
-ma_date <- 1713052800
-as.Date(ma_date, origin = "1970-01-01")
+# ma_date <- 1713052800
+# as.Date(ma_date, origin = "1970-01-01")
 
-d <- as.Date("2024-04-14")
-dput(d)
-
-operation <- OPERATION %>% 
-  mutate(date2 = Date_operation / 86400,
-         date = as.Date(date2))
 
 operation <- operation %>% 
-    
+  mutate(Date_operation = Date_operation / 86400,
+         Date_operation = as.Date(Date_operation))
+
+operation <- operation %>% 
+  mutate(Strate_arboree_RG = str_detect(string = Strates_ripisylve_RD, pattern = "Arborée"),
+         Strate_arbustive_RG = str_detect(string = Strates_ripisylve_RD, pattern = "Arbustive"),
+         Strate_herbacee_RG = str_detect(string = Strates_ripisylve_RD, pattern = "Herbacée"),
+         Absence_ripisylve_RG = str_detect(string = Strates_ripisylve_RD, pattern = "Absence"),
+         Strate_arboree_RD = str_detect(string = Strates_ripisylve_RD, pattern = "Arborée"),
+         Strate_arbustive_RD = str_detect(string = Strates_ripisylve_RD, pattern = "Arbustive"),
+         Strate_herbacee_RD = str_detect(string = Strates_ripisylve_RD, pattern = "Herbacée"),
+         Absence_ripisylve_RD = str_detect(string = Strates_ripisylve_RD, pattern = "Absence")) %>% 
+  select(-Strates_ripisylve_RG,  -Strates_ripisylve_RD) %>% 
+  mutate(Nb_petits_bois = ifelse(Nb_petits_bois == "nan", NA, Nb_petits_bois),
+         Nb_petits_bois = as.numeric(Nb_petits_bois),
+         Nb_blocs = ifelse(Nb_blocs == "nan", NA, Nb_blocs),
+         Nb_blocs = as.numeric(Nb_blocs))
+
+
+
+
+
+
+# vrac essais
+operation3 <- operation %>% 
+  cg_unnest()
+
+df <- operation   %>% 
+ unnest_wider(pH, names_sep='_')
+
+my_nested_vars_index <- map_lgl(.x = names(df),
+                                .f = ~is.list(df[[.]]))
+my_nested_vars <- names(df)[my_nested_vars_index]
+
+l <- df$Strates_ripisylve_RG
+str_detect(l, pattern = "Herb")
+
+removeL(l)
+test <- appendL(l)
+test <- as.character(l)
+
