943bdfa11383ce3a1366c89a212ff104c8fa0a14
IRZ Pascal <pascal.irz@ofb.gouv.fr>
2026-06-03 16:15:58 +0200

    clean dataframes

--- scripts/10_get_data_with_gristapi.R
+++ scripts/10_get_data_with_gristapi.R
@@ -18,18 +18,22 @@ api <- grist_api$new(
 
 fetch_grist_db(api)
 
-operation <- operation %>% 
-  mutate(Date_operation = Date_operation / 86400,
-         Date_operation = as.Date(Date_operation))
 
-operation <- operation %>% 
-  split_list_col(var_id = Operation, var_split = Strates_ripisylve_RG) %>% 
-  split_list_col(var_id = Operation, var_split = Strates_ripisylve_RD) %>% 
-  select(-Strates_ripisylve_RG, -Strates_ripisylve_RD) %>% 
-  mutate(Nb_petits_bois = ifelse(Nb_petits_bois == "nan", NA, Nb_petits_bois),
-         Nb_petits_bois = as.numeric(Nb_petits_bois),
-         Nb_blocs = ifelse(Nb_blocs == "nan", NA, Nb_blocs),
-         Nb_blocs = as.numeric(Nb_blocs))
+
+# operation2 <- operation %>% 
+#   split_list_col(var_id = Operation, var_split = Strates_ripisylve_RG) %>% 
+#   split_list_col(var_id = Operation, var_split = Strates_ripisylve_RD) %>% 
+#   select(-Strates_ripisylve_RG, -Strates_ripisylve_RD) %>% 
+#   mutate(Nb_petits_bois = ifelse(Nb_petits_bois == "nan", NA, Nb_petits_bois),
+#          Nb_petits_bois = as.numeric(Nb_petits_bois),
+#          Nb_blocs = ifelse(Nb_blocs == "nan", NA, Nb_blocs),
+#          Nb_blocs = as.numeric(Nb_blocs))
+
+
+         
+         # Nb_petits_bois = as.numeric(Nb_petits_bois),
+         # Nb_blocs = ifelse(Nb_blocs == "nan", NA, Nb_blocs),
+         # Nb_blocs = as.numeric(Nb_blocs))
 
 my_dfs <- Filter(function(x) is.data.frame(get(x)) , ls())
 
